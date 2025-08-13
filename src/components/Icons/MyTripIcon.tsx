7f3eaa6a267c5caa3665868718602f8f855bcd87
LeeJinyoung218 <amy021899@gmail.com>
2025-08-13 14:50:51 +0900

    chore: 아이콘 추가 및 수정

--- src/components/Icons/MyTripIcon.tsx
+++ src/components/Icons/MyTripIcon.tsx
@@ -15,7 +15,7 @@ const MyTripIcon: React.FC<MyTripIconProps> = ({
     <svg 
       width={size} 
       height={size} 
-      viewBox="0 0 36 36" 
+      viewBox={`0 0 ${size} ${size}`}
       fill="none" 
       xmlns="http://www.w3.org/2000/svg"
       className={className}
