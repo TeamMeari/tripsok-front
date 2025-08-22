f291d33af03ec0b77afd11dbc38af9ae0bb8d250
LeeJinyoung218 <amy021899@gmail.com>
2025-08-22 13:22:12 +0900

    feat: Header, MenuApp Layout 위치

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,26 @@ import MainPage from './pages/MainPage';
 import ListPage from './pages/ListPage';
 import ContentPage from './pages/ContentPage';
 import SearchListPage from './pages/SearchListPage';
+import Header from './components/header/Header';
+import MenuApp from './components/MenuApp';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/list" element={<ListPage />} />
-          <Route path="/content/:id" element={<ContentPage />} />
-          <Route path="/search" element={<SearchListPage />} />
-        </Routes>
-      </BrowserRouter>
+      <div className="app-area">
+        <BrowserRouter>
+          <Header />
+          <div className="content-area">
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/list" element={<ListPage />} />
+              <Route path="/content/:id" element={<ContentPage />} />
+              <Route path="/search" element={<SearchListPage />} />
+            </Routes>
+          </div>
+          <MenuApp />
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
