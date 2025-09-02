e26c2326e1aa95d57eb072855c4c2cbcacdd6252
SemiHyeon <106103995+SemiHyeon@users.noreply.github.com>
2025-09-02 16:31:41 +0900

    feat: globe 아이콘 버튼 타입 추가

--- src/components/common/Button/IconBtn.tsx
+++ src/components/common/Button/IconBtn.tsx
@@ -1,21 +1,35 @@
 import React from "react";
 import { ArrowLeft, Search } from "lucide-react";
 import styles from "./IconBtn.module.css";
+import GlobeIcon from "../../Icons/globeIcon";
 
-type IconType = "search" | "arrow";
+type IconType = "search" | "arrow" | "globeIcon";
+type ColorType = "white" | "gray";
 
 interface IconButtonProps {
     type: IconType;
+    color?: ColorType; // 기본값은 white
     onClick?: () => void;
 }
 
-const IconButton: React.FC<IconButtonProps> = ({ type, onClick }) => {
+const IconButton: React.FC<IconButtonProps> = ({ type, color = "white", onClick }) => {
     return (
-        <button className={styles.iconBtn} onClick={onClick}>
+        <button
+            className={`${styles.iconBtn} ${color === "gray" ? styles.gray : ""}`}
+            onClick={onClick}
+        >
             {type === "search" && <Search className={styles.icon} size={29} />}
             {type === "arrow" && <ArrowLeft className={styles.icon} size={29} />}
+            {type === "globeIcon" && (
+                <GlobeIcon
+                    className={styles.icon}
+                    size={29}
+                    color={color === "gray" ? "#888888" : "#ffffff"}
+                />
+            )}
         </button>
     );
 };
+
 
 export default IconButton;
