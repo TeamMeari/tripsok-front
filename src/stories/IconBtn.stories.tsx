66818705b93dede9e11e7f9544640cbe0e805043
SemiHyeon <106103995+SemiHyeon@users.noreply.github.com>
2025-09-02 16:31:41 +0900

    feat: globe 아이콘 버튼 타입 추가

--- src/stories/IconBtn.stories.tsx
+++ src/stories/IconBtn.stories.tsx
@@ -10,5 +10,9 @@ export const MultipleButtons = () => (
     <div style={{ display: "flex", gap: "16px", alignItems: "center" }}>
         <IconButton type="search" />
         <IconButton type="arrow" />
+        <IconButton type="arrow" color="gray"/>
+        <IconButton type="globeIcon" />
+        <IconButton type="globeIcon" color="gray" />
+
     </div>
 );
