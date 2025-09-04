cbe8bc4355e88326310520724b182c8e3671ab0a
LeeJinyoung218 <amy021899@gmail.com>
2025-09-04 10:07:48 +0900

    feat: BannerCarousel에 제목 및 인디케이터 추가

--- src/components/feature/Carousel/BannerCarousel.tsx
+++ src/components/feature/Carousel/BannerCarousel.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import styles from "./BannerCarousel.module.css";
 import { animate } from "@motionone/dom";
 import { useScrollToSlide } from "../../../hooks/useScrollToSlide";
+import { useTranslation } from "react-i18next";
 
 interface BannerType {
     url: string
@@ -13,6 +14,7 @@ interface BannerCarouselProps {
 }
 
 const BannerCarousel = ({ banners }: BannerCarouselProps) => {
+    const { t } = useTranslation();
     const [currentIndex, setCurrentIndex] = useState<number>(0);
     const [isAnimating, setIsAnimating] = useState<boolean>(false);
     const currentInnerWidth = useRef<number>(0);
@@ -56,13 +58,16 @@ const BannerCarousel = ({ banners }: BannerCarouselProps) => {
     }, [window.innerWidth]);
 
     return (
-        <div className={styles.carouselTrack}>
-            <div className={styles.carouselItems} ref={carouselRef}>
-                {banners.map((banner, key) => 
-                    <div key={key} className={styles.carouselItem}>
-                        {banner && <img src={banner.image} alt="banner" />}
-                    </div>
-                )}
+        <div>
+            <p className={styles.bannerTitle}>{t("mainNoMissEvent")} <span>{currentIndex + 1}/{banners.length}</span></p>
+            <div className={styles.carouselTrack}>
+                <div className={styles.carouselItems} ref={carouselRef}>
+                    {banners.map((banner, key) => 
+                        <div key={key} className={styles.carouselItem}>
+                            {banner && <img src={banner.image} alt="banner" />}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
