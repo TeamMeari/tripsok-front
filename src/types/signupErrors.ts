9ce3fc37687824799ad277d013706beaec011aa9
LeeJinyoung218 <amy021899@gmail.com>
2025-09-10 11:23:39 +0900

    fix: 언어 재렌더링 오류 수정

--- src/types/signupErrors.ts
+++ src/types/signupErrors.ts
@@ -1,32 +1,30 @@
-import i18n from "../i18n";
-
 // 이메일 인증
-export const INVALID_EMAIL = () => i18n.t("invalidEmail");
-export const EXISTING_EMAIL = () => i18n.t("existingAccount");
+export const INVALID_EMAIL = "invalidEmail";
+export const EXISTING_EMAIL = "existingAccount";
 
 // 인증 코드
-export const EXPIRED_CODE = () => i18n.t("expiredCode");
-export const INVALID_CODE = () => i18n.t("codeMismatch");
+export const EXPIRED_CODE = "expiredCode";
+export const INVALID_CODE = "codeMismatch";
 
 // 비밀번호
 export const PASSWORD_VALIDATION_WARNING = (password: string) => {
     if (password.length < 8) {
-        return i18n.t("passwordTooShort");
+        return "passwordTooShort";
     }
     const notExisting = [];
     if (!password.match(/[a-zA-Z]/)) {
-        notExisting.push(i18n.t("letterRequired"));
+        notExisting.push("letterRequired");
     }
     if (!password.match(/[0-9]/)) {
-        notExisting.push(i18n.t("numberRequired"));
+        notExisting.push("numberRequired");
     }
     if (!password.match(/[@$!%*#?&]/)) {
-        notExisting.push(i18n.t("specialCharRequired"));
+        notExisting.push("specialCharRequired");
     }
     if (notExisting.length > 0) {
-        return i18n.t("passwordValidationWarning", { condition: notExisting.join(", ") });
+        return "passwordValidationWarning";
     }
     return "";
 };
 
-export const PASSWORD_CONFIRM_ERROR = () => i18n.t("passwordMismatch");
+export const PASSWORD_CONFIRM_ERROR = "passwordMismatch";
