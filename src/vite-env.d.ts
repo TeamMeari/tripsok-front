f351fe283dd9692e5e809d98a682d988c271e4e2
LeeJinyoung218 <amy021899@gmail.com>
2025-09-10 14:40:16 +0900

    feat: Google API 연동을 위한 클라이언트 ID 및 리다이렉트 URI 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
     VITE_PUBLIC_API_BASE_URL: string | undefined;
     readonly VITE_KAKAOMAP_KEY: string;
+    readonly VITE_PUBLIC_GOOGLE_CLIENT_ID: string;
+    readonly VITE_PUBLIC_GOOGLE_REDIRECT_URI: string;
 }
 
 interface ImportMeta {
