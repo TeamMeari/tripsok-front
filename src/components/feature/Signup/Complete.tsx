3947c2314a2d4bc0161dda607bb735f75cd9b40d
LeeJinyoung218 <amy021899@gmail.com>
2025-09-10 12:19:19 +0900

    fix: 환영 메시지를 Trans 컴포넌트를 사용하여 안전하게 렌더링하도록 수정

--- src/components/feature/Signup/Complete.tsx
+++ src/components/feature/Signup/Complete.tsx
@@ -1,6 +1,6 @@
 import styles from "../../../pages/SignupPage.module.css";
 import Button from "../../common/Button/CommonBtn";
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 
 interface CompleteProps {
     nickname: string;
@@ -13,8 +13,9 @@ const Complete = ({ nickname }: CompleteProps) => {
         <div className={styles.step}>
             <p
                 className={styles.message}
-                dangerouslySetInnerHTML={{ __html: t("welcomeNickname", { name: nickname }) }}
-            />
+            >
+                <Trans i18nKey="welcomeNickname" components={{ br: <br />, span: <span /> }} values={{ name: nickname }} />
+            </p>
             <div className={styles.buttonFixedTab}>
                 <a href="/">
                     <Button borderRadius="12px" size="large">{t("home")}</Button>
