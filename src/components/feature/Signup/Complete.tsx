8c2a67344e5a1d73ca88f0921ecaa162ca0caa91
LeeJinyoung218 <amy021899@gmail.com>
2025-09-10 12:29:41 +0900

    feat: 회원가입 마지막 페이지에 해시태그 버튼 추가

--- src/components/feature/Signup/Complete.tsx
+++ src/components/feature/Signup/Complete.tsx
@@ -1,13 +1,30 @@
 import styles from "../../../pages/SignupPage.module.css";
 import Button from "../../common/Button/CommonBtn";
 import { useTranslation, Trans } from "react-i18next";
-
+import { Tag } from "../../../types/Tag";
+import { useState, useEffect } from "react";
+import { useApi } from "../../../hooks/useApi";
+import HashtagButton from "../../common/HashtagBtn";
 interface CompleteProps {
     nickname: string;
 }
 
 const Complete = ({ nickname }: CompleteProps) => {
     const { t } = useTranslation();
+    const { apiCall, isLoading } = useApi();
+    const [hashtags, setHashtags] = useState<Tag[]>([]);
+
+    const fetchHashtags = () => {
+        apiCall('/theme', 'GET').then(response => {
+            if (response.status === 200) {
+                setHashtags(response.data as Tag[]);
+            }
+        });
+    }
+
+    useEffect(() => {
+        fetchHashtags();
+    }, []);
 
     return (
         <div className={styles.step}>
@@ -16,6 +33,13 @@ const Complete = ({ nickname }: CompleteProps) => {
             >
                 <Trans i18nKey="welcomeNickname" components={{ br: <br />, span: <span /> }} values={{ name: nickname }} />
             </p>
+            <div className={styles.content}>
+                <div className={styles.hashtagsContainer}>
+                    {hashtags.map(hashtag => (
+                        <HashtagButton key={hashtag.id} label={hashtag.type} />
+                    ))}
+                </div>
+            </div>
             <div className={styles.buttonFixedTab}>
                 <a href="/">
                     <Button borderRadius="12px" size="large">{t("home")}</Button>
