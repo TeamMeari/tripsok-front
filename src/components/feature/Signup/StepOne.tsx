0169a469812d81395d3dd524360c1e3e47ad8ce6
LeeJinyoung218 <amy021899@gmail.com>
2025-09-11 09:40:26 +0900

    fix: 이메일 인증 API 응답 상태 코드를 200에서 204로 수정

--- src/components/feature/Signup/StepOne.tsx
+++ src/components/feature/Signup/StepOne.tsx
@@ -27,7 +27,7 @@ const StepOne = ({ onSubmit }: StepOneProps) => {
     // 이메일 코드 보내기
     const handleEmailVerification = useCallback(() => {
         apiCall("/auth/email/send", "POST", { email }).then(response => {
-            if (response.status === 200) {
+            if (response.status === 204) {
                 onSubmit(email);
             } else {
                 setEmailError(EXISTING_EMAIL);
