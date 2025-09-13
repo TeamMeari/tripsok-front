024b058aa5efa4ee237c1d8007a253d23b75eba2
LeeJinyoung218 <amy021899@gmail.com>
2025-09-13 17:15:51 +0900

    fix: storybook 오류 수정(Router 문제)

--- src/stories/MenuApp.stories.tsx
+++ src/stories/MenuApp.stories.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import MenuApp from "../components/MenuApp";
+import { BrowserRouter } from "react-router-dom";
 
 export default {
     title: "Components/MenuApp",
     component: MenuApp,
 };
 
-export const DefaultMenu = () => <MenuApp />;
+export const DefaultMenu = () => <BrowserRouter><MenuApp /></BrowserRouter>;
