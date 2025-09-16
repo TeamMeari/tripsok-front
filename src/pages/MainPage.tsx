eb136906ac53d8580a157d39a4cff4264ebe1146
LeeJinyoung218 <amy021899@gmail.com>
2025-09-16 23:34:14 +0900

    fix: MainPage api 연동 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,14 +3,13 @@ import MainCarousel from '../components/feature/Carousel/MainCarousel';
 import { useEffect, useState } from 'react';
 import MenuTab from '../components/feature/Tab/MenuTab';
 import menuTabs from '../types/menuTabs';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import CardCarousel from '../components/feature/Carousel/CardCarousel';
 import Footer from '../components/Footer';
 import BannerCarousel from '../components/feature/Carousel/BannerCarousel';
 import { getDailyKeywords } from '../utils/keywordSelector';
 import { useApi } from '../hooks/useApi';
 import CardType from '../types/Card';
-import { BannerType } from '../components/feature/Carousel/BannerCarousel';
 import { MainCarouselItem } from '../components/feature/Carousel/MainCarousel';
 import { PlacesResponse, Place } from '../types/apiResponse';
 
@@ -22,12 +21,10 @@ const MainPage = () => {
   const [top5CardCarouselItems, setTop5CardCarouselItems] = useState<CardType[]>([]);
   const [firstCardCarouselItems, setFirstCardCarouselItems] = useState<CardType[]>([]);
   const [secondCardCarouselItems, setSecondCardCarouselItems] = useState<CardType[]>([]);
-  const [bannerCarouselItems, setBannerCarouselItems] = useState<BannerType[]>([]);
   const { apiCall: MainCarouselApiCall, isLoading: MainCarouselIsLoading } = useApi();
   const { apiCall: Top5CardCarouselApiCall, isLoading: Top5CardCarouselIsLoading } = useApi();
   const { apiCall: FirstCardCarouselApiCall, isLoading: FirstCardCarouselIsLoading } = useApi();
   const { apiCall: SecondCardCarouselApiCall, isLoading: SecondCardCarouselIsLoading } = useApi();
-  const { apiCall: BannerCarouselApiCall, isLoading: BannerCarouselIsLoading } = useApi();
 
   const handleTabClick = (tab: number) => {
     setActiveTab(tab);
@@ -35,7 +32,7 @@ const MainPage = () => {
 
   const fetchMainCarouselPlaces = () => {
     // keyword 하나 사용
-    const queryParams = `?page=1&size=5&sortKey=rank&direction=desc&locale=${i18n.language}&typeSearch=embedding&categoryFilter=false&q=${dailyKeywords[0]}`;
+    const queryParams = `?page=0&size=10&sortKey=rank&direction=desc&locale=${i18n.language}&typeSearch=text&categoryFilter=false&q=${dailyKeywords[0]}`;
     MainCarouselApiCall<PlacesResponse>("/places/accommodation" + queryParams, "GET").then((response) => {
       if (response.status === 200) {
         if (response.data) {
@@ -49,11 +46,11 @@ const MainPage = () => {
   }
 
   const fetchTop5CardCarouselPlaces = () => {
-    const queryParams = `?page=1&size=5&sortKey=rank&direction=desc&locale=${i18n.language}&typeSearch=embedding&categoryFilter=false&q=${dailyKeywords[1]}`;
+    const queryParams = `?page=0&size=10&sortKey=rank&direction=desc&locale=${i18n.language}&categoryFilter=false&q=%EA%B0%95%EB%A6%89`;
     Top5CardCarouselApiCall<PlacesResponse>("/places/accommodation" + queryParams, "GET").then((response) => {
       if (response.status === 200) {
         if (response.data) {
-          setTop5CardCarouselItems(response.data.items.map((v: Place, idx: number) => ({
+          setTop5CardCarouselItems(response.data.items.slice(0, 5).map((v: Place, idx: number) => ({
             id: v.id,
             image: v.thumbnailUrl,
             rank: idx + 1,
@@ -67,7 +64,7 @@ const MainPage = () => {
 
   const fetchFirstCardCarouselPlaces = () => {
     // keyword 하나 사용
-    const queryParams = `?page=1&size=5&sortKey=rank&direction=desc&locale=${i18n.language}&typeSearch=embedding&categoryFilter=false&q=${dailyKeywords[1]}`;
+    const queryParams = `?page=0&size=10&sortKey=rank&direction=desc&locale=${i18n.language}&typeSearch=text&categoryFilter=false&q=${dailyKeywords[1]}`;
     FirstCardCarouselApiCall<PlacesResponse>("/places/accommodation" + queryParams, "GET").then((response) => {
       if (response.status === 200) {
         if (response.data) {
@@ -84,7 +81,7 @@ const MainPage = () => {
 
   const fetchSecondCardCarouselPlaces = () => {
     // keyword 하나 사용
-    const queryParams = `?page=1&size=5&sortKey=rank&direction=desc&locale=${i18n.language}&typeSearch=embedding&categoryFilter=false&q=${dailyKeywords[2]}`;
+    const queryParams = `?page=0&size=10&sortKey=rank&direction=desc&locale=${i18n.language}&typeSearch=text&categoryFilter=false&q=${dailyKeywords[2]}`;
     SecondCardCarouselApiCall<PlacesResponse>("/places/accommodation" + queryParams, "GET").then((response) => {
       if (response.status === 200) {
         if (response.data) {
@@ -99,10 +96,6 @@ const MainPage = () => {
     });
   }
 
-  const fetchBannerCarouselPlaces = () => {
-
-  }
-
   const texts = [
     `목적지를 검색하고\n나만의 여행을 시작해보세요.`,
     `강릉으로 떠나는\n가장 쉬운 방법.`,
@@ -110,13 +103,28 @@ const MainPage = () => {
     `여행의 모든 순간을\n함께 만들어가요.`,
   ]
 
-  // useEffect(() => {
-  //   fetchMainCarouselPlaces();
-  //   fetchTop5CardCarouselPlaces();
-  //   fetchFirstCardCarouselPlaces();
-  //   fetchSecondCardCarouselPlaces();
-  //   fetchBannerCarouselPlaces();
-  // }, []);
+  // 수정 필요
+  const bannerCarouselItems = [
+    {
+      url: "/",
+      image: "https://cdn.pixabay.com/photo/2025/06/13/14/48/bird-9658215_1280.jpg",
+    },
+    {
+      url: "/",
+      image: "https://cdn.pixabay.com/photo/2022/11/17/09/49/fog-7597710_1280.jpg",
+    },
+    {
+      url: "/",
+      image: "https://cdn.pixabay.com/photo/2020/11/04/18/59/leaves-5713290_1280.jpg",
+    }
+  ]
+
+  useEffect(() => {
+    fetchMainCarouselPlaces();
+    fetchTop5CardCarouselPlaces();
+    fetchFirstCardCarouselPlaces();
+    fetchSecondCardCarouselPlaces();
+  }, []);
 
   return <div className={styles.page}>
     <MainCarousel
@@ -128,11 +136,11 @@ const MainPage = () => {
     <MenuTab tabs={menuTabs} activeTab={activeTab} isIcon={true} tabOnClick={handleTabClick} />
     <div className={styles.section1}>
       <div className={styles.cardList}>
-        <TitleLink text={t("mainTopSpots")} link="/list" />
+        <TitleLink i18nKey="mainTopSpots" link="/list" />
         <CardCarousel cards={top5CardCarouselItems} isLoading={Top5CardCarouselIsLoading} />
       </div>
       <div className={styles.cardList}>
-        <TitleLink text={t("mainKeywordSpotsFirst")} link="/list" />
+        <TitleLink i18nKey="mainKeywordSpotsFirst" values={{ keyword: dailyKeywords[1] }} link="/list" />
         <CardCarousel cards={firstCardCarouselItems} isLoading={FirstCardCarouselIsLoading} />
       </div>
       <div className={styles.listLinkContainer}>
@@ -140,9 +148,9 @@ const MainPage = () => {
       </div>
     </div>
     <div className={styles.section2}>
-      <BannerCarousel banners={bannerCarouselItems} isLoading={BannerCarouselIsLoading} />
+      <BannerCarousel banners={bannerCarouselItems} />
       <div className={styles.cardList}>
-        <TitleLink text={t("mainKeywordSpotsSecond")} link="/list" />
+        <TitleLink i18nKey="mainKeywordSpotsSecond" values={{ keyword: dailyKeywords[2] }} link="/list" />
         <CardCarousel cards={secondCardCarouselItems} isLoading={SecondCardCarouselIsLoading} />
       </div>
     </div>
@@ -150,9 +158,9 @@ const MainPage = () => {
   </div>;
 };
 
-const TitleLink = ({ text, link }: { text: string, link: string }) => {
+const TitleLink = ({ i18nKey, values, link }: { i18nKey: string, values?: any, link: string }) => {
   return <a href={link} className={styles.titleLink}>
-    {text} <span>&gt;</span>
+    {<Trans i18nKey={i18nKey} values={values} />} <span>&gt;</span>
   </a>
 }
 
