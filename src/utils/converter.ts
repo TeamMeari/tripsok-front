8e3849bf40306d2d4b49aa3724bf2e95a657b148
SemiHyeon <106103995+SemiHyeon@users.noreply.github.com>
2025-09-18 23:52:23 +0900

    refactor: Type 대소문자 설정 변경

--- src/utils/converter.ts
+++ src/utils/converter.ts
@@ -2,5 +2,6 @@ export const convertTypeToLowerCase = (type: string) => {
     if (type === "RESTAURANT") return "restaurant";
     if (type === "ACCOMMODATION") return "accommodation";
     if (type === "TOUR") return "tour";
+    if (type === "TOURIST_SPOT") return "tour";
     return type;
 }
