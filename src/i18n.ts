64b7962c9db5b613ca401b7852ed0f8bf28d370a
LeeJinyoung218 <amy021899@gmail.com>
2025-10-29 12:31:49 +0900

    fix: 검색창 플레이스홀더 i18n 추가

--- src/i18n.ts
+++ src/i18n.ts
@@ -116,6 +116,8 @@ const resources = {
             mainCarouselText2: "강릉으로 떠나는\n가장 쉬운 방법.",
             mainCarouselText3: "강릉, 그 설레는 여정을\n시작하세요.",
             mainCarouselText4: "여행의 모든 순간을\n함께 만들어가요.",
+            // 검색창
+            searchInputPlaceholder : "검색어를 입력해주세요.",
             // keywordSelector 텍스트
             gangneung: "강릉",
             sea: "바다",
@@ -301,6 +303,8 @@ const resources = {
             mainCarouselText2: "The easiest way to go to Gangneung",
             mainCarouselText3: "Start your journey with Gangneung,\nthe journey of excitement.",
             mainCarouselText4: "Create every moment of your journey\ntogether.",
+            // 검색창
+            searchInputPlaceholder : "Please enter your search term.",
             // keywordSelector 텍스트
             gangneung: "Gangneung",
             sea: "sea",
@@ -485,6 +489,8 @@ const resources = {
             mainCarouselText2: "江陵への最も簡単な方法",
             mainCarouselText3: "江陵での旅を始めましょう,\nその興奮の旅。",
             mainCarouselText4: "旅のすべての瞬間を\n一緒に作りましょう。",
+            // 검색창
+            searchInputPlaceholder : "検索語を入力してください。",
             // keywordSelector 텍스트
             gangneung: "江陵",
             sea: "海",
@@ -669,6 +675,8 @@ const resources = {
             mainCarouselText2: "前往江陵的最简单方式",
             mainCarouselText3: "开始您的旅程，\n充满兴奋的旅程。",
             mainCarouselText4: "共同创造旅行的每一刻",
+            // 검색창
+            searchInputPlaceholder : "请输入搜索词。",
             // keywordSelector 텍스트
             gangneung: "江陵",
             sea: "海洋",
